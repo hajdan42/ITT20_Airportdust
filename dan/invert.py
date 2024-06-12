134ae4a2e5c2e63251cf82643a46a066fdaf18c0
Hdani <hajnaldani1@gmail.com>
2024-06-12 13:19:04 +0100

    scipy linalg solve added

--- dan/invert.py
+++ dan/invert.py
@@ -2,8 +2,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import scipy as scp
+
+method="pseudo"
+
+# "pseudo", 
 
 
 #load engine location data
+y=np.random.rand(5*20)
+A=np.random.rand(20*5,298*5)
 
 
+if(method=="pseudo"):
+	x=scp.sparse.linalg.lsqr(A, y)
+	print(x)
+
+print("HI")
